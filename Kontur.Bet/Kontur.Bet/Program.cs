6bd34a175aa9d3515186b68bdb4ef9d3f208f88e
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-06-23 23:54:18 +0500

    no message

--- Kontur.Bet/Kontur.Bet/Program.cs
+++ Kontur.Bet/Kontur.Bet/Program.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 
 internal class Program
 {
+    private static int defaultTimeout = 120000;
     private static ILocator scoreLocator = null!;
     private static ILocator koefLocator = null!;
     private static ILocator team1btn1 = null!;
@@ -24,12 +25,17 @@ internal class Program
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync();
         page = await browser.NewPageAsync();
-        await page.GotoAsync(matchList, new() { Timeout = 60000 });
+        await page.GotoAsync(matchList, new() { Timeout = defaultTimeout });
 
         var matchLinks = new List<string>();
         var elements = await page.Locator(matchLinksPath).ElementHandlesAsync();
         foreach (var elementTask in elements)
         {
+            var textLink = await elementTask.InnerTextAsync();
+            if (textLink.Contains("Home") || textLink.Contains("Away") || 
+                textLink.Contains("Хозяева") || textLink.Contains("Гости"))
+                continue;
+
             var matchLink = await elementTask.GetAttributeAsync("href");
             matchLinks.Add("https://www.fon.bet" + matchLink!);
         }
@@ -56,7 +62,7 @@ internal class Program
         const string scorePath = templateBtnPath + $"div[2]/div[1]";
         const string koefPath = templateBtnPath + $"div[2]/div[2]/div/div/div";
 
-        await page.GotoAsync(matchLink, new() { Timeout = 60000 });
+        await page.GotoAsync(matchLink, new() { Timeout = defaultTimeout });
         scoreLocator = page.Locator(scorePath);
         koefLocator = page.Locator(koefPath);
         team1btn1 = page.Locator(team1btn1Path);
@@ -80,22 +86,23 @@ internal class Program
             }
         }
 
-        var randomScore = GetRandomScore(matchKoefs);
-        Console.WriteLine($"{await page.Locator(team1Path).InnerTextAsync(new() { Timeout = 60000 })} - " +
-                          $"{await page.Locator(team2Path).InnerTextAsync(new() { Timeout = 60000 })} , " +
+        var randomScore = GetScoreByMinKoef(matchKoefs);
+        // var randomScore = GetRandomScore(matchKoefs);
+        Console.WriteLine($"{await page.Locator(team1Path).InnerTextAsync(new() { Timeout = defaultTimeout })} - " +
+                          $"{await page.Locator(team2Path).InnerTextAsync(new() { Timeout = defaultTimeout })} , " +
                           $"{randomScore.Item1.Item1}:{randomScore.Item1.Item2} , " +
                           $"{randomScore.Item2}");
     }
 
     private static async Task<(int, int)> GetMatchScore()
     {
-        var arr = (await scoreLocator.InnerTextAsync(new() { Timeout = 60000 })).Split("–");
+        var arr = (await scoreLocator.InnerTextAsync(new() { Timeout = defaultTimeout })).Split("–");
         return (int.Parse(arr[0].Trim()), int.Parse(arr[1].Trim()));
     }
 
     private static async Task<decimal> GetMatchKoef()
     {
-        if (decimal.TryParse(await koefLocator.InnerTextAsync(new() { Timeout = 60000 }), CultureInfo.InvariantCulture, out decimal result))
+        if (decimal.TryParse(await koefLocator.InnerTextAsync(new() { Timeout = defaultTimeout }), CultureInfo.InvariantCulture, out decimal result))
             return result;
         return -1;
     }
@@ -118,14 +125,14 @@ internal class Program
                 if (!await ButtonEnabled(team1btn1))
                     return false;
 
-                await team1btn1.ClickAsync(new() { Timeout = 60000 });
+                await team1btn1.ClickAsync(new() { Timeout = defaultTimeout });
             }
             else
             {
                 if (!await ButtonEnabled(team1btn2))
                     return false;
 
-                await team1btn2.ClickAsync(new() { Timeout = 60000 });
+                await team1btn2.ClickAsync(new() { Timeout = defaultTimeout });
             }
 
             (currentScore, _) = await GetMatchScore();
@@ -145,14 +152,14 @@ internal class Program
                 if (!await ButtonEnabled(team2btn1))
                     return false;
 
-                await team2btn1.ClickAsync(new() { Timeout = 60000 });
+                await team2btn1.ClickAsync(new() { Timeout = defaultTimeout });
             }
             else
             {
                 if (!await ButtonEnabled(team2btn2))
                     return false;
 
-                await team2btn2.ClickAsync(new() { Timeout = 60000 });
+                await team2btn2.ClickAsync(new() { Timeout = defaultTimeout });
             }
 
             (_, currentScore) = await GetMatchScore();
@@ -164,6 +171,14 @@ internal class Program
     private static async Task<bool> ButtonEnabled(ILocator btn)
     {
         return (await btn.GetAttributeAsync("class"))!.Contains("enabled");
+    }
+
+    private static ((int, int), decimal) GetScoreByMinKoef(Dictionary<(int, int), decimal> koefs)
+    {
+        var minKoef = koefs.Min(x => x.Value);
+        var minKoefs = koefs.Where(x => x.Value == minKoef).ToArray();
+        var index = new Random().Next(minKoefs.Length);
+        return (minKoefs[index].Key, minKoefs[index].Value);
     }
 
     private static ((int, int), decimal) GetRandomScore(Dictionary<(int, int), decimal> koefs)
